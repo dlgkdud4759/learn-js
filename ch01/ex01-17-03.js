7f20c12705892836495e022a16e0e2452e192a49
Lee HaYeong <dlgkdud4759@naver.com>
2025-09-24 23:59:29 +0900

    if-else문을 활용한 프로그램

---
+++ ch01/ex01-17-03.js
@@ -0,0 +1,64 @@
+/*
+if 문을 이용하여 사용자의 로그인 상태와 사용자의 등급을 출력하세요.
+*/
+
+const isLogin = true; // 로그인 상태
+const isUser = true; // 구매자
+const isSeller = false; // 판매자
+const isAdmin = false; // 관리자
+
+// 로그인 상태 출력
+// 로그인 상태/로그아웃 상태
+
+if (isLogin) {
+  console.log("로그인 상태");
+} else {
+  console.log("로그아웃 상태");
+}
+
+// 사용자 유형 출력
+// 구매자/판매자/관리자
+
+if (isUser) {
+  console.log(구매자);
+} else {
+  if (isSeller) {
+    console.log(판매자);
+  } else {
+    if (isAdmin) {
+      console.log(관리자);
+    }
+  }
+}
+
+// 로그인 상태와 사용자 유형을 조합해서 출력
+// 사용자 로그인 상태/사용자 로그아웃 상태
+// 판매자 로그인 상태/판매자 로그아웃 상태
+// 관리자 로그인 상태/관리자 로그아웃 상태
+if (isLogin && isUser) {
+  console.log("구매자 로그인 상태");
+} else {
+  console.log("구매자 로그아웃 상태");
+}
+
+if (isLogin && isSeller) {
+  console.log("판매자 로그인 상태");
+} else {
+  console.log("판매자 로그아웃 상태");
+}
+
+if (isLogin && isAdmin) {
+  console.log("관리자 로그인 상태");
+} else {
+  console.log("관리자 로그아웃 상태");
+}
+
+/* 정아님 코드
+//관리자 로그인상태/관리자 로그아웃상태
+if (isLogin === true && isAdmin === true) {
+  console.log('관리자 로그인상태');
+}
+if (isLogin === false && isAdmin === true) {
+  console.log('관리자 로그아웃상태');
+}
+*/
