fd24ba72e32b9d2b738edcd9672f74613f80d9a8
Lee HaYeong <dlgkdud4759@naver.com>
2025-09-24 23:55:01 +0900

    자바스크립트 사용해보기

---
+++ ch01/ex01-04.js
@@ -0,0 +1,12 @@
+// 일반적인 로그 메시지 출력
+console.log("Hello World!");
+
+// 경고 메시지 출력 (브라우저에서는 노란색으로 표시)
+console.warn("Hello");
+
+// 디버그 메시지 출력
+console.debug("World!!");
+console.log("Hello");
+
+// 에러 메시지 출력
+console.error("World!!!");
