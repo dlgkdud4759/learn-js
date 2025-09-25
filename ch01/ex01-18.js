447251df7211a8102fd46abeb524580c3f079bd0
dlgkdud4759 <dlgkdud4759@naver.com>
2025-09-25 11:42:03 +0900

    if-else if 문

--- ch01/ex01-18.js
+++ ch01/ex01-18.js
@@ -2,3 +2,27 @@
 if-else if문
 */
 
+// 나이에 따른 성인/미성년자 구분
+
+const num = 1;
+
+if (num > 0) {
+  console.log("양수");
+} else if (num < 0) {
+  console.log("음수");
+} else {
+  console.log("영");
+}
+
+const num2 = 15;
+
+// num2가 2의 배수이면 '2의 배수' 출력하고,
+// 3의 배수이면 '3의 배수' 출력하고, 그렇지 않으면 '2와 3의 배수가 아님' 출력
+
+if (num2 % 2 === 0) {
+  console.log("2의 배수");
+} else if (num2 % 3 === 0) {
+  console.log("3의 배수");
+} else {
+  console.log("2와 3의 배수가 아님");
+}
