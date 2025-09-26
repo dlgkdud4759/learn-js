66f9e7b0fc1af3dbb8f8b76c1ab2dc347b6679b2
Lee HaYeong <dlgkdud4759@naver.com>
2025-09-26 16:15:41 +0900

    for 반복문 - break, continue 활용

--- ch01/ex01-22.js
+++ ch01/ex01-22.js
@@ -2,3 +2,37 @@
 반복문 - break, continue
 */
 
+const n = 1;
+const m = 10;
+let result = null;
+
+// n ~ m 까지의 정수 중에서 가장 작은 짝수를 찾아서 출력하세요.
+// for (let i = m; i >= n; i--) {
+//   if (i % 2 === 0) {
+//     result = i;
+//   }
+// }
+
+for (let i = n; i <= m; i++) {
+  if (i % 2 === 0) {
+    result = i;
+    break;
+  }
+}
+console.log(`${n} ~ ${m} 정수 중 가장 작은 짝수: `, result);
+
+// n ~ m 까지의 정수 중에서 홀수만 찾아서 출력하세요.
+for (let i = n; i <= m; i++) {
+  if (i % 2 !== 0) {
+  }
+  console.log("홀수: ", i);
+}
+
+// n ~ m 까지의 정수 중에서 홀수와 짝수를 찾아서 출력하세요.
+for (let i = n; i <= m; i++) {
+  if (i % 2 !== 0) {
+    console.log("홀수: ", i);
+    continue;
+  }
+  console.log("짝수:", i);
+}
