73b53a8a85252aa2c9683a9f798f10eab6488093
Lee HaYeong <dlgkdud4759@naver.com>
2025-09-26 10:42:05 +0900

    while 반복문

--- ch01/ex01-20.js
+++ ch01/ex01-20.js
@@ -8,3 +8,16 @@ let i = 0;
 while (i <= 10) {
   console.log(i++);
 }
+
+// 1부터 10까지의 모든 수의 합계를 출력
+let sum = 0;
+let j = 0;
+console.log("j\tsum");
+
+while (j <= 10) {
+  sum += j;
+  console.log(`${i}\t${sum}`);
+  j++;
+}
+
+console.log("합계", sum);
