6971ad41e5137a017226967acd1929dcebf7ff59
Lee HaYeong <dlgkdud4759@naver.com>
2025-09-26 16:15:54 +0900

    object 객체 생성

--- ch01/ex01-23.js
+++ ch01/ex01-23.js
@@ -2,5 +2,14 @@
 new Object() - 생성자 함수로 객체 생성
 */
 
+const user = new Object(); // object 생성자 함수로 객체 생성
 
+// 점 표기법
+user.name = "이하영"; // user 객체에 name 속성을 추가
+user.age = 25; // user 객체에 age 속성을 추가
+user.hobby = "게임"; // user 객체에 hobby 속성을 추가
 
+// 대괄호 표기법
+user["hobby"] = "게임"; // user 객체에 hobby 속성을 추가
+
+console.log(user.name, user["age"], user.hobby);
