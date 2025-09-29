853a37879655e02ffff8ad417724b6789cc7edca
Lee HaYeong <dlgkdud4759@naver.com>
2025-09-29 10:51:50 +0900

    별찍기 및 구구단 과제

--- ch01/ex01-21-02.js
+++ ch01/ex01-21-02.js
@@ -32,3 +32,40 @@ for (let i = 0; i < lines; i++) {
   }
   console.log(star);
 }
+
+// 과제 별찍기 - 1
+for (let i = 0; i < lines; i++) {
+  let star = "";
+  for (let j = 1; j < lines - i; j++) {
+    star += " ";
+  }
+  for (let k = 0; k <= i; k++) {
+    star += "*";
+  }
+  console.log(star);
+}
+
+// 과제 별찍기 - 2
+for (let i = 0; i < lines; i++) {
+  let star = "";
+  for (let k = 1; k <= i; k++) {
+    star += " ";
+  }
+  for (let j = 1; j <= lines - i; j++) {
+    star += "*";
+  }
+  console.log(star);
+}
+
+// 과제 별찍기 - 3
+for (let i = 1; i <= lines; i++) {
+  let space = "";
+  let star = "";
+  for (let k = 0; k < lines - i; k++) {
+    space += " ";
+  }
+  for (let k = 0; k < i * 2 - 1; k++) {
+    star += "*";
+  }
+  console.log(space + star);
+}
