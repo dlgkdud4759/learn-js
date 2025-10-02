6f0092934c2ba02c4ee70381c1f6c1c2097d5cde
Lee HaYeong <dlgkdud4759@naver.com>
2025-10-02 13:41:26 +0900

    구조 분해 할당

--- ch01/ex01-41.js
+++ ch01/ex01-41.js
@@ -2,5 +2,24 @@
 구조 분해 할당(Destructuring assignment)
 */
 
+const foo = ["yellow", "green", "blue"];
 
+// const one = foo[0];
+// const two = foo[1];
+// const three = foo[2];
 
+// 구조분해할당
+const [one, two, three] = foo;
+
+// const [user, setUser] = useState(); // 배열을 반환 (React 방식)
+
+console.log(one, two, three); // 'yellow', 'green', 'blue'
+
+const user = { userName: "구분할핑", userAge: 30 };
+// const userName = user.userName;
+// const userAge = user.userAge;
+
+// 구조분해할당(객체)
+const { userName, userAge } = user;
+
+console.log(userName, userAge); // 구분할핑 30
