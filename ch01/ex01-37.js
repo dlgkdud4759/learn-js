702658ba329db2df57624ba5dfb2fbb164a87f46
Lee HaYeong <dlgkdud4759@naver.com>
2025-10-02 13:14:57 +0900

    Call by Value와 Reference 차이

--- ch01/ex01-37.js
+++ ch01/ex01-37.js
@@ -10,24 +10,30 @@ Call by Value와 Call by Reference 차이
  * @param {Array} data2
  */
 // 내가 만든 코드
-function add10(data1, data2) {
-  console.log("함수 내부", data1 + 10, data2[0] + 10);
-}
+// function add10(data1, data2) {
+//   console.log("함수 내부", data1 + 10, data2[0] + 10);
+// }
 
-add10(80, [80]);
+// add10(80, [80]);
 
 // 강사님 코드
 function add10(data1, data2) {
   data1 += 10;
   data2[0] += 10;
   console.log("함수 내부", data1, data2[0]); // 90 90
+  // return data1; 수정한 값을 전달하고 싶을 때
 }
 
+// 이 변수는 선언과 동시에 stack 메모리에 저장됨
 let d1 = 80; // number - 원시 타입
+
+// 참조형 데이터 타입들은 heap 메모리에 저장됨
 let d2 = [80]; // array(object) - 참조 타입
 
 console.log("함수 호출 이전", d1, d2[0]); // 80 80
 
-add10(80, [80]);
+add10(d1, d2);
+
+// d1 = add10(d1, d2); // add10에서 수정한 값을 사용하고 싶을 때
 
 console.log("함수 호출 이전", d1, d2[0]); // 80 90
