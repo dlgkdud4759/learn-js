79e301c696ad85e2786f5b8d2582ac243c41884d
Lee HaYeong <dlgkdud4759@naver.com>
2025-10-14 10:34:24 +0900

    변수 호이스팅

--- ch02/ex02-12.js
+++ ch02/ex02-12.js
@@ -4,6 +4,9 @@
   - let, const로 선언한 변수의 경우 호이스팅 단계에서 선언은 되지만 초기화가 되지 않기 때문에 
     선언 전에 접근하면 에러 발생 (undefined 값도 할당되지 않은 상태)
 */
+console.log(a); // undefined
+console.log(b); // 에러 ReferenceError: Cannot access 'b' before initialization
+console.log(c); // 에러 ReferenceError: Cannot access 'c' before initialization
 
 var a = 10; // 호이스팅 단계에서 undefined로 초기화 됨
 let b = 20; // 호이스팅 단계에서 선언만 되고 초기화가 안됨
