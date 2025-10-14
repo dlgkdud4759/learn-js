91d51b2d07ce8987a48ed4158564644a372a04b5
Lee HaYeong <dlgkdud4759@naver.com>
2025-10-14 10:33:58 +0900

    화살표 함수

--- ch02/ex02-10.js
+++ ch02/ex02-10.js
@@ -8,27 +8,31 @@
 */
 
 // 기존 함수(표현식)
-let add = function(x, y) {
+let add = function (x, y) {
   return x + y;
 };
 
 // 화살표 함수
-
+add = (x, y) => {
+  return x + y;
+};
 
 // 화살표 함수 축약
-
+add = (x, y) => x + y;
 
 console.log(add(10, 20));
 
 // 기존 함수
-let add10 = function(x) {
+let add10 = function (x) {
   return x + 10;
 };
 
 // 화살표 함수
-
+add10 = (x) => {
+  return x + 10;
+};
 
 // 화살표 함수 축약
-
+add10 = (x) => x + 10;
 
 console.log(add10(100));
