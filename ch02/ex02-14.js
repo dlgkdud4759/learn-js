0a201733a5b16d0bd6e6179a73bd4f3119009fe3
Lee HaYeong <dlgkdud4759@naver.com>
2025-10-14 15:12:12 +0900

    함수 호출 방법 1 - 일반 함수

--- ch02/ex02-14.js
+++ ch02/ex02-14.js
@@ -3,13 +3,19 @@
   - this는 window 객체
 */
 
-function f1(){
-  
+// 선언문
+function f1() {
+  // console.log(this);
+  window.console.log("window.");
+  this.console.log("this.");
+  console.log("hello");
+}
+
+// 표현식
+const f2 = function () {
+  console.log(this);
 };
 
-const f2 = function(){
-  
-};
-
+// 일반 함수로 호출
 f1();
 f2();
