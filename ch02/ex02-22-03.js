e29a1ebb830dd75c855a8917eb559dd58091ceb8
Lee HaYeong <dlgkdud4759@naver.com>
2025-10-16 17:22:54 +0900

    배열 다루기 - 배열의 다양한 메서드(고차함수) 사용

--- ch02/ex02-22-03.js
+++ ch02/ex02-22-03.js
@@ -1,66 +1,96 @@
 /*
-* 배열 다루기 - 배열의 다양한 메서드(고차함수) 사용
-*/
+ * 배열 다루기 - 배열의 다양한 메서드(고차함수) 사용
+ */
 
 const todoList = [
   {
-    "id": 1,
-    "title": "JavaScript 공부",
-    "done": true
+    id: 1,
+    title: "JavaScript 공부",
+    done: true,
   },
   {
-    "id": 4,
-    "title": "React 공부",
-    "done": false
+    id: 4,
+    title: "React 공부",
+    done: false,
   },
   {
-    "id": 3,
-    "title": "바닐라 프로젝트",
-    "done": false
+    id: 3,
+    title: "바닐라 프로젝트",
+    done: false,
   },
   {
-    "id": 2,
-    "title": "TypeScript 공부",
-    "done": false
+    id: 2,
+    title: "TypeScript 공부",
+    done: false,
   },
   {
-    "id": 5,
-    "title": "Final 프로젝트",
-    "done": true
-  }
+    id: 5,
+    title: "Final 프로젝트",
+    done: true,
+  },
 ];
 
 // 완료된 할일 목록
-let doneList = [];
+// Array.prototype.filter: 콜백 함수가 true를 리턴하는 요소만 모아서 새로운 배열로 반환
+// 화살표 함수 "() => {}" 에서 {} 를 제외한다면 리턴값을 가지게 된다.
+let doneList = todoList.filter((todoItem) => todoItem.done);
 
-console.log('완료된 할일 목록', doneList);
+console.log("완료된 할일 목록", doneList);
 
 // 남은 할일 목록
-let remainList = [];
+let remainList = todoList.filter((todoItem) => !todoItem.done);
 
-console.log('남은 할일 목록', remainList);
+console.log("남은 할일 목록", remainList);
 
 // 남은 할일 수
-let remainCount = 0;
+let remainCount = todoList.filter((todoItem) => !todoItem.done).length;
 
-console.log('남은 할일 수', remainCount);
+console.log("남은 할일 수", remainCount);
 
 // id=2인 할일
-let todo = null;
+// Array.prototype.find: 콜백 함수가 true를 리턴하는 첫 번째 요소를 반환
+let todo = todoList.find((todoItem) => todoItem.id === 2);
 
-console.log('id=2인 할일', todo);
+console.log("id=2인 할일", todo);
 
 // id=3인 할일의 index
-let todoIndex = -1;
+// Array.prototype.findIndex: 콜백 함수가 true를 리턴하는 첫 번째 요소를 반환
+let todoIndex = todoList.findIndex((todoItem) => todoItem.id === 3);
 
-console.log('id=3인 할일의 index', todoIndex);
+// todoList.find((todoItem, i) => {
+//   if (todoItem.id === 3) {
+//     todoIndex = i;
+//     return true;
+//   }
+// });
+
+console.log("id=3인 할일의 index", todoIndex);
 
 // 남은 할일이 하나라도 있는가?
-let hasTodo = false;
+// Array.prototype.some: 콜백 함수가 true를 리턴하는 경우가 있으면 true를 반환
+let hasTodo = todoList.some((todoItem) => !todoItem.done);
 
-console.log('남은 할일이 하나라도 있는가?', hasTodo);
+console.log("남은 할일이 하나라도 있는가?", hasTodo);
 
 // 할일이 모두 완료 되었는가?
-let isAllDone = true;
+// Array.prototype.every: 콜백 함수가 전부 true를 리턴하는 경우 true를 반환
+let isAllDone = todoList.every((todoItem) => todoItem.done);
 
-console.log('할일이 모두 완료 되었는가?', isAllDone);
+console.log("할일이 모두 완료 되었는가?", isAllDone);
+
+// 모든 할일에 대해서 title만 꺼내서 소문자로 변경
+// Array.prototype.map: 콜백 함수의 리턴값을 모아서 새로운 배열로 반환
+let lowerTitleList = todoList.map((todoItem) => {
+  return todoItem.title.toLocaleLowerCase(); // ['javascript 공부', 'react 공부', ...]
+});
+
+console.log("할일 title(소문자)", lowerTitleList);
+
+// 완료된 할일에 대해서 title만 추출
+let doneTitleList = [];
+
+doneTitleList = doneList.map((todoItem) => {
+  return todoItem.title;
+});
+
+console.log("완료된 할일 title", doneTitleList);
