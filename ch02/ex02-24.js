a8d64c53a82d54a3e934cc988e37885729b7468b
dlgkdud4759 <dlgkdud4759@naver.com>
2025-10-17 11:36:04 +0900

    즉시실행 함수

--- ch02/ex02-24.js
+++ ch02/ex02-24.js
@@ -4,3 +4,21 @@
 
 */
 
+// 의도치 않게 전역 변수 alert을 재정의함
+// var alert = "alert 변수명은 포기 못해!!!";
+// console.log(alert);
+
+// 일반 함수
+// fn1 전역변수가 생김
+var fn1 = function () {
+  var alert = "alert 변수명은 포기 못해!!!";
+  console.log(alert);
+};
+
+fn1();
+
+// 즉시 실행 함수
+(function () {
+  var alert = "alert 변수명은 포기 못해!!!";
+  console.log("IIFE", alert);
+})();
