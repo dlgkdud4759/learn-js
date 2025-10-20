1bdb09392d2b62495b2e80c70aa8b86e23a980ca
dlgkdud4759 <dlgkdud4759@naver.com>
2025-10-20 09:07:09 +0900

    클래스 정의와 상속(private class fields)

--- ch03/ex03-05-02.js
+++ ch03/ex03-05-02.js
@@ -5,3 +5,24 @@
 
 */
 
+// HighSchool 클래스 정의
+class HighSchool {
+  #kor = 0;
+  #eng = 0;
+
+  constructor(kor, eng) {
+    this.#kor = kor;
+    this.#eng = eng;
+  }
+  #sum() {
+    return this.#kor + this.#eng;
+  }
+  avg() {
+    return Math.round(this.sum() / 2);
+  }
+}
+
+const h1 = new HighSchool(100, 91);
+console.log("h1", h1.kor, h1.eng); // 100, 91
+console.log("h1 총점", h1.sum()); // 191
+console.log("h1 평균", h1.avg()); // 95.5
