463b5c381c1fac175c51f09e591ed1f5f6039723
dlgkdud4759 <dlgkdud4759@naver.com>
2025-10-20 09:10:38 +0900

    Partial 메서드 추가

--- js/mylib.js
+++ js/mylib.js
@@ -1,5 +1,16 @@
 const mylib = {};
 
+// 함수를 partial application으로 만들어주는 partial 메서드 추가
+// [100]
+Function.prototype.partial = function (...preArgs) {
+  const fn = this; // sum
+  // sum100 - [20]
+  return function (...nextArgs) {
+    const args = [...preArgs, ...nextArgs]; // [100, 20]
+    return fn(...args); // sum(100, 20)
+  };
+};
+
 // Child가 Parent를 상속받는다.
 mylib.inherite = function (Parent, Child) {
   // const F = new Function();
