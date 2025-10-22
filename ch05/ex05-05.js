57c19cb05f975efc8d2ad17dfc63656e71cd1b59
Lee HaYeong <dlgkdud4759@naver.com>
2025-10-22 10:50:25 +0900

    Todo List - 삭제 버튼 기능 추가

--- ch05/ex05-05.js
+++ ch05/ex05-05.js
@@ -77,7 +77,7 @@ function getTodoItemElem(item) {
   deleteElem.appendChild(deleteTxt);
 
   // <li data-no="2">
-  liElem.setAttribute("data-no", "2");
+  liElem.setAttribute("data-no", item.id);
 
   /*
   <li data-no="2">
@@ -93,6 +93,15 @@ function getTodoItemElem(item) {
   </li>
   */
   liElem.appendChild(titleElem);
+
+  // 삭제 이벤트 추가
+  deleteElem.addEventListener("click", function () {
+    // this: 이벤트가 발생한 요소(<button>)
+    const parentLi = this.parentNode; // <button>의 부모인 <li>
+    const no = parentLi.getAttribute("data-no"); // <li data-no=""> 속성 추출
+
+    removeItem(no);
+  });
 
   /*
   <li data-no="2">
@@ -144,6 +153,15 @@ function addItem(title) {
  */
 function handleKeyup(event) {
   if (event.key === "Enter") add();
+}
+
+/**
+ * Todo 아이템 하나를 삭제하는 함수
+ * @param {number} no - 삭제할 Todo 아이템의 번호(id)
+ */
+function removeItem(no) {
+  const targetLi = document.querySelector(`.todolist > li[data-no = "${no}"]`);
+  targetLi?.remove();
 }
 
 // '추가' 버튼 클릭
