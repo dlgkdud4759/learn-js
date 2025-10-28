2e78b032078cb8bf38ca896e4a68fc1b0624cabf
Lee HaYeong <dlgkdud4759@naver.com>
2025-10-28 15:02:16 +0900

    TypeScript - 타입 별칭으로 객체의 타입 선언

---
+++ ch06/ex06-07.js
@@ -0,0 +1,8 @@
+// 타입 별칭으로 객체의 타입 선언
+(() => {
+    const haru = { name: "haru", age: 5 };
+    const namu = { name: "namu", age: 8 };
+    console.log(haru);
+    console.log(namu);
+})();
+export {};
