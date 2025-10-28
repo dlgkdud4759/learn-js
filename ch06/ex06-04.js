6cf7496b2f8e9e5d14064759a1d84b599bb2f380
Lee HaYeong <dlgkdud4759@naver.com>
2025-10-28 14:28:50 +0900

    TypeScript - 선택적 파라미터(?)

---
+++ ch06/ex06-04.js
@@ -0,0 +1,10 @@
+// 선택적 파라미터(optional parameter)
+(() => {
+    // age에 ?을 붙이면서 age 값을 받지 않아도 console.log가 찍히게 됨
+    function user(name, age) {
+        console.log(name, age);
+    }
+    user("하루", 5);
+    user("나무");
+})();
+export {};
--- ch06/ex06-04.ts
+++ ch06/ex06-04.ts
@@ -1,10 +1,11 @@
-// // 선택적 파라미터(optional parameter)
+// 선택적 파라미터(optional parameter)
 
-// (()=>{
-//   function user(name, age) {
-//     console.log(name, age);
-//   }
+(() => {
+  // age에 ?을 붙이면서 age 값을 받지 않아도 console.log가 찍히게 됨
+  function user(name: string, age?: number) {
+    console.log(name, age);
+  }
 
-//   user('하루', 5);
-
-// })();
+  user("하루", 5);
+  user("나무");
+})();
