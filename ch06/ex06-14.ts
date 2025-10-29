a0673d3803b7c96e00ee36ca21d29facfd940f24
Lee HaYeong <dlgkdud4759@naver.com>
2025-10-29 11:50:49 +0900

    TypeScript - 인터페이스 상속(계층 구조 및 다중 상속)

--- ch06/ex06-14.ts
+++ ch06/ex06-14.ts
@@ -1,6 +1,48 @@
 // 인터페이스 상속 - 계층 구조
 // ex06-13.ts 복사
 
-(()=>{
-  
+(() => {
+  // 할일 등록시 사용
+  interface TodoRegist {
+    title: string;
+    content: string;
+  }
+
+  // 할일 상세 조회시 사용
+  interface TodoInfo extends TodoRegist {
+    readonly id: number;
+    done?: boolean;
+  }
+
+  interface TodoInfoWithDate extends TodoInfo {
+    createdAt: Date;
+    updateAt: Date;
+  }
+
+  // 할일 등록에 사용
+  const todo1: TodoRegist = {
+    title: "할일1",
+    content: "내용1",
+  };
+
+  // 할일 상세 조회에 사용
+  const todo2: TodoInfo = {
+    id: 5,
+    title: "할일2",
+    content: "내용2",
+    done: true,
+  };
+
+  const todo3: TodoInfoWithDate = {
+    id: 7,
+    title: "할일3",
+    content: "내용3",
+    done: true,
+    createdAt: new Date(),
+    updateAt: new Date(),
+  };
+
+  console.log(todo1);
+  console.log(todo2);
+  console.log(todo3);
 })();
