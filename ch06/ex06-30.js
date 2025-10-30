0d42b0ca16673cdf7aafc7a570a25db9fcb2f9d9
Lee HaYeong <dlgkdud4759@naver.com>
2025-10-30 14:45:26 +0900

    TypeScript - 누락된 js 파일 추가

--- ch06/ex06-30.js
+++ ch06/ex06-30.js
@@ -1,4 +1,16 @@
 // 유틸리티 타입 - Required
 (() => {
+    const todo1 = {
+        title: "할일1",
+        // content: "내용1",
+        // done: false,
+    };
+    const todo2 = {
+        title: "할일2",
+        content: "내용2",
+        done: true,
+    };
+    console.log(todo1);
+    console.log(todo2);
 })();
 export {};
