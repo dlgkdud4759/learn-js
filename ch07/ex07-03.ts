cb60cd2e33019b1b75332087bf62d52dc8917b5b
Lee HaYeong <dlgkdud4759@naver.com>
2025-10-31 10:51:42 +0900

    TypeScript - 내장 함수(serInterval, clearInterval)

--- ch07/ex07-03.ts
+++ ch07/ex07-03.ts
@@ -1,8 +1,27 @@
 // 주요 내장 함수 - setInterval, clearInterval
 // ex07-02.ts 복사
 
-(()=>{
-  
+(() => {
+  console.log("시작");
+
+  let count = 0;
+
+  const timerId = setInterval(
+    (n1: number, n2: number) => {
+      count++;
+
+      // 3번 실행 후 중지
+      if (count === 3) {
+        // Timer 작업 취소
+        clearInterval(timerId);
+      }
+
+      console.log("3초 마다 실행", n1 + n2, new Date());
+    },
+    1000 * 3,
+    10,
+    20
+  );
+
+  console.log("종료");
 })();
-
-
