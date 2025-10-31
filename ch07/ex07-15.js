9893c2e95f4f55bb8c3b752004956e4a424b4d75
Lee HaYeong <dlgkdud4759@naver.com>
2025-10-31 17:57:16 +0900

    TypeScript - 내장 객체(concat, indexOf, lastIndexOf, forEach, map, find, findIndex, some, every, reduce)

--- ch07/ex07-15.js
+++ ch07/ex07-15.js
@@ -1,9 +1,17 @@
 // 내장 객체 Array - indexOf(), lastIndexOf()
 (() => {
     const arr = [1, 3, 5, 8, 9, 3, 4, 5];
-    console.log('첫번째 3의 위치'); // 1
-    console.log('마지막 3의 위치'); // 5
-    console.log('첫번째 2의 위치'); // -1
-    console.log('마지막 2의 위치'); // -1
+    const arr1 = [1, 3, 5, 8, 9, 3, 4, 5];
+    console.log("첫번째 3의 위치", arr.indexOf(3)); // 1
+    console.log("마지막 3의 위치", arr.lastIndexOf(3)); // 5
+    console.log("첫번째 2의 위치", arr.indexOf(2)); // -1
+    console.log("마지막 2의 위치", arr.lastIndexOf(2)); // -1
+    // 배열에서 8을 찾아서 제거
+    arr.splice(arr.indexOf(8), 1);
+    console.log(arr);
+    // 원본은 수정하지 않고 8이 제거된 새로운 배열 생성
+    const arr2 = [...arr1];
+    arr2.splice(arr2.indexOf(8), 1);
+    console.log(arr1, arr2);
 })();
 export {};
--- ch07/ex07-16.js
+++ ch07/ex07-16.js
@@ -3,11 +3,18 @@
     const arr = [10, 20, 30];
     // FIXME arr의 각 요소를 제곱하여 새로운 배열을 생성하는 코드를 작성하세요.(for문)
     let newArr = [];
-    console.log('for문', newArr);
+    for (let elem of arr) {
+        newArr.push(elem * elem);
+    }
+    console.log("for문", newArr);
     // FIXME arr의 각 요소를 제곱하여 새로운 배열을 생성하는 코드를 작성하세요.(forEach)
     newArr = [];
-    console.log('forEach', newArr); // [100, 400, 900]
+    arr.forEach((elem) => {
+        newArr.push(elem * elem);
+    });
+    console.log("forEach", newArr); // [100, 400, 900]
     // FIXME arr의 각 요소를 제곱하여 새로운 배열을 생성하는 코드를 작성하세요.(map)
-    console.log('map', newArr); // [100, 400, 900]
+    newArr = arr.map((elem) => elem * elem);
+    console.log("map", newArr); // [100, 400, 900]
 })();
 export {};
