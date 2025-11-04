2596df9ddbb0d5d4cd6a974eae5c599820f12659
Lee HaYeong <dlgkdud4759@naver.com>
2025-11-04 14:45:10 +0900

    TypeScript - 비동기 함수(async, await)

--- ch08/ex08-11.ts
+++ ch08/ex08-11.ts
@@ -1,27 +1,48 @@
 // 비동기 함수 - async 사용
 // 비동기 함수의 작업이 완료된 후 실행할 코드가 있으면 async/await 사용
 
-(()=>{
-  function p1(){
-    
+(() => {
+  function p1(): Promise<string> {
+    return new Promise((resolve, reject) => {
+      resolve("p1 정상 완료");
+    });
   }
-  function a1(){
-    
-  }
-
-  function p2(){
-    
-  }
-  function a2(){
-    
+  async function a1(): Promise<string> {
+    return "a1 정상 완료";
   }
 
-  function test(){
-    
+  function p2(): Promise<string> {
+    return new Promise((resolve, reject) => {
+      reject("p2 실행 오류");
+    });
+  }
+  async function a2(): Promise<string> {
+    throw "a2 실행 오류";
   }
 
-  console.log('1. 작업 시작.');
+  function test() {
+    p1()
+      .then((result) => {
+        console.log(result);
+      })
+      .then(a1)
+      .then((result) => {
+        console.log(result);
+      })
+      // .then(p2)
+      // .then((result) => {
+      //   console.log(result);
+      // })
+      .then(a2)
+      .then((result) => {
+        console.log(result);
+      })
+      .catch((reason) => console.log(reason));
+  }
+
+  console.log("1. 작업 시작.");
   test();
-  console.log('2. 작업 종료. 이 메세지가 f1 작업 완료 보다 먼저 나오면 비동기로 호출된다는 의미');
-
+  console.log(
+    "2. 작업 종료. 이 메세지가 f1 작업 완료 보다 먼저 나오면 비동기로 호출된다는 의미"
+  );
 })();
