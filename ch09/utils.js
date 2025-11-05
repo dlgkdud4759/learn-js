44e4a9c9adafe7ba51ed46c4d4870e9997276e13
Lee HaYeong <dlgkdud4759@naver.com>
2025-11-05 17:18:07 +0900

    TypeScript - utils(appendImages 추가)

---
+++ ch09/utils.js
@@ -0,0 +1,17 @@
+// 이미지 목록을 받아서 화면에 추가
+export function appendImages(images) {
+    const liList = images.map((item) => {
+        const li = document.createElement("li"); // <li>
+        const img = document.createElement("img"); // <img>
+        img.src = item.url; // <img src="https://????.???">
+        img.height = 200; // <img src="https://????.???" height="200">
+        img.alt = "고양이"; // <img src="https://????.???" height="200" alt="고양이">
+        li.appendChild(img); // <li><img src="https://????.???" height="200" alt="고양이"></li>
+        return li;
+    });
+    const catList = document.querySelector("#cat-list");
+    // 맨 앞에 추가
+    catList?.prepend(...liList); //<ul><li><img src="https://????.???" height="200" alt="고양이"></li>...</ul>
+    // 맨 끝에 추가
+    // catList?.append(...liList); //<ul><li><img src="https://????.???" height="200" alt="고양이"></li>...</ul>
+}
